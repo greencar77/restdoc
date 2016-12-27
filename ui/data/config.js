474cd699605787649ae8736e0592e93160bdae6e
Karlis Coka <karlisc@inbox.lv>
2016-12-27 12:09:21 +0200

    links to classes

--- ui/data/config.js
+++ ui/data/config.js
@@ -1,13 +1,13 @@
 //end with slash
 var sourcePrefix = 'src/main/java/';
-var gitSource = 'https://github.com/x/tree/master/';
+var gitSource = 'https://github.com/greencar77/restdoc/tree/master/';
 
 var javaClasses; //Java classes
 
 
 function resolveGitSource(className) {
-    if (className.startsWith('com')) {
-        return gitSource + 'x/' + sourcePrefix + className.replace(/\./g, '/') + '.java';
+    if (className.startsWith('com.rabarbers.restdoc.mockproj')) {
+        return gitSource + 'restdocmock/' + sourcePrefix + className.replace(/\./g, '/') + '.java';
     } else if (className.startsWith('uuu')) {
         return gitSource + 'x/' + sourcePrefix + className.replace(/\./g, '/') + '.java';
     } else {
